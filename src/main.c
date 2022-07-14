d05254357c27df3187c4165bce3c5c12daf5212f
Tiemen <tiemen88@gmail.com>
2022-07-14 20:07:11 +0200

    main

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: tiemen <tiemen@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/13 15:52:01 by tiemen        #+#    #+#                 */
-/*   Updated: 2022/07/14 14:17:28 by tiemen        ########   odam.nl         */
+/*   Updated: 2022/07/14 20:06:11 by tiemen        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	parse_line(char *line)
+char	***parse_line(char *line)
 {
 	int		pipe_count;
 	int i;
@@ -22,14 +22,16 @@ int	parse_line(char *line)
 	pipe_count = 0;
 	i = 0;
 	split_pipe = ft_split(line, '|');
-	while (split_pipe[i] != NULL)
+	while (split_pipe[pipe_count] != NULL)
 		pipe_count++;
-	cmd_arr = malloc(sizeof(char **) * pipe_count);
+	cmd_arr = malloc(sizeof(char **) * pipe_count + 1);
 	while (i < pipe_count)
 	{
 		cmd_arr[i] = ft_split(split_pipe[i], ' ');
 		i++;
 	}
+	cmd_arr[i] = NULL;
+	return (cmd_arr);
 	
 }
 
@@ -40,17 +42,36 @@ int	parse_line(char *line)
 
 int	main(int argc, char **argv, char **envp)
 {
-	HIST_ENTRY **the_history_list;
+	// HIST_ENTRY **the_history_list;
 	char	*str;
 	char	**root_path;
 	//char	*cmd_path;
+	int		i, j;
+	char	***cmd_arr;
 	
+	i = 0;
 	(void) argc;
 	(void) argv;
 	root_path = find_path(envp);
 	str = readline("SuperShell: ");
-	printf("%s\n", root_path[0]);
+	while (root_path[i])
+	{
+		printf("%s\n", root_path[i]);
+		i++;
+	}
+	i = 0;
 	printf("readline:%s\n", str);
+	cmd_arr = parse_line(str);
+	while (cmd_arr[i])
+	{
+		j = 0;
+		while (cmd_arr[i][j])
+		{
+			printf("%s\n", cmd_arr[i][j]);
+			j++;
+		}
+		i++;
+	}
 	free_stuff(str);
 
 	

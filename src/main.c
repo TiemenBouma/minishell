57fbaa1d99df7caa5407738d4e43065fe9b8da8b
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2022-07-19 21:43:12 +0200

    made part of parser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tbouma <tbouma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:52:01 by tiemen            #+#    #+#             */
-/*   Updated: 2022/07/19 15:49:33 by tbouma           ###   ########.fr       */
+/*   Updated: 2022/07/19 21:42:49 by tbouma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,72 @@
 	
 // }
 
+int	get_cmd_line_count(char **tokens, int *cmd_count)
+{
+	int i;
+
+	i = 0;
+	while (tokens[i] != NULL)
+	{
+		if (tokens[i][0] == '|')
+			*cmd_count++;
+		i++;
+	}
+	return (0);
+}
+
+int	get_cmd_len(char **tokens, int *current_token)
+{
+	int len;
+
+	len = 0;
+	while (tokens[*current_token] != '|')
+	{
+		len++;
+		*current_token++;
+	}
+	return (len);
+}
+
+int	make_cmd(char **cmd_line, char **tokens, int current_token, int len)
+{
+	int i;
+
+	i = 0;
+	while (i < len)
+	{
+		cmd_line[i] = malloc(sizeof(char) * (ft_strlen(tokens[current_token + i]) + 1));
+		i ++;
+	}
+	//COPY str
+}
+
+get_cmds(char **tokens, char ***cmds, int cmd_lines)
+{
+	int	current_token;
+	int len;
+	int	i;
+
+	i = 0;
+	current_token = 0;
+	while (i < cmd_lines)
+	{
+		len = get_cmd_len(tokens, &current_token);
+		cmds[i] = malloc(sizeof(char *) * (len + 1); //need to malloc one more *
+		make_cmd(cmds[i], tokens, current_token, len);
+	}
+}
+
 make_cmd_and_redir(char **tokens)
 {
+	int cmd_lines;
+	char	***cmds;
+
+	cmd_lines = 1;
+	get_cmd_line_count(tokens, &cmd_lines);	
+	cmds = malloc(sizeof(char **) * (cmd_lines + 1));
+	cmds[cmd_lines] = NULL;
+	get_cmds(tokens, cmds, cmd_lines);
 	
 }
 

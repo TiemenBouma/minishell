3b89ad7f32f2c73feb18af8b0440238b068098e1
tiemen <tiemen88@gmail.com>
2022-09-06 12:52:48 +0200

    added +1 back to global var in malloc

--- src/parsing_cmd_lines.c
+++ src/parsing_cmd_lines.c
@@ -94,7 +94,7 @@ char	***make_cmd_lines(char **all_tokens)
 	cmd_lines = 1;
 	cmd_lines = get_cmd_line_count(all_tokens, cmd_lines);	
 	cmds = malloc(sizeof(char **) * (cmd_lines + 1));
-	g_pipe_heredoc = malloc(sizeof(int *) * (cmd_lines)); // +1 is for sending the current pipe as first value, I know it is super dirty. (TIEMEN)
+	g_pipe_heredoc = malloc(sizeof(int *) * (cmd_lines + 1)); // +1 is for sending the current pipe as first value, I know it is super dirty. (TIEMEN)
 	if(g_pipe_heredoc == NULL)
 		return (NULL); //ERROR malloc (TIEMEN)
 	while (i < cmd_lines)

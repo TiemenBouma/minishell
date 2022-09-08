9e492ee1dd1f7fa190c3f18535a54cd70ffbd488
dkcb <yes@exa.com>
2022-09-08 14:05:06 +0200

    exec norm

--- src/execution.c
+++ src/execution.c
@@ -6,13 +6,13 @@
 /*   By: tbouma <tbouma@student.42.fr>                +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/17 16:53:02 by dkocob        #+#    #+#                 */
-/*   Updated: 2022/09/08 13:23:57 by dkocob        ########   odam.nl         */
+/*   Updated: 2022/09/08 14:04:16 by dkocob        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-extern int **g_pipe_heredoc;
+extern int	**g_pipe_heredoc;
 
 static void	execve_error(char *path, int error, char *envpath)
 {
@@ -24,21 +24,20 @@ static void	execve_error(char *path, int error, char *envpath)
 	}
 	else if (error == ENOENT)
 	{
-
-			ft_putstr_fd("minishell: ", 2);
-			ft_putstr_fd(path, 2);
-			ft_putstr_fd(": command not found\n", 2);
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": command not found\n", 2);
 	}
 	else
-		{
-			ft_putstr_fd("minishell: ", 2);
-			ft_putstr_fd(path, 2);
-			ft_putstr_fd(": execve error\n", 2);
-		}
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": execve error\n", 2);
+	}
 	exit (127);
 }
 
-void ft_exec_child(struct	s_main *main_struct,  struct s_cmd_info	*curr_cmd, char *path)
+void	ft_exec_child(struct s_main *main_struct, struct s_cmd_info *curr_cmd)
 {
 	if (is_builtin(curr_cmd->exec.exec_line[0]) < 7 && curr_cmd->set_file_err == 0)
 	{
@@ -50,17 +49,16 @@ void ft_exec_child(struct	s_main *main_struct,  struct s_cmd_info	*curr_cmd, cha
 		curr_cmd->arr_env_list = make_arr_from_list(&main_struct->env_llist);
 		if (execve(curr_cmd->exec.exec_line[0], curr_cmd->exec.exec_line, curr_cmd->arr_env_list) == -1)
 		{
-			if(curr_cmd->exec.exec_line[0] == NULL)
+			if (curr_cmd->exec.exec_line[0] == NULL)
 				exit(0);
-			path = find_var_in_list(&main_struct->env_llist, "PATH");
-			execve_error(curr_cmd->exec.exec_line[0], errno, path);
+			execve_error(curr_cmd->exec.exec_line[0], errno, find_var_in_list(&main_struct->env_llist, "PATH"));
 		}
 	}
 	else
 		exit(126);
 }
 
-void ft_child(struct	s_main *main_struct, int (*p)[2][2], int i, int id, struct s_cmd_info	*curr_cmd, char *path)
+void	ft_child(struct s_main *main_struct, int (*p)[2][2], int i, int id, struct s_cmd_info *curr_cmd)
 {
 	if ((id == 0 && check_buildin_fork(curr_cmd) == 1) || (id == 0 && main_struct->cmd_count > 1))
 	{
@@ -72,59 +70,57 @@ void ft_child(struct	s_main *main_struct, int (*p)[2][2], int i, int id, struct 
 		else if (curr_cmd->has_infile == 2 || i == 1)
 			err_chk(dup2(curr_cmd->exec.fd_in, S_IN), 2, "");
 		else
-			err_chk(dup2((*p)[PREV][P_OUT], S_IN), 2, "");	
+			err_chk(dup2((*p)[PREV][P_OUT], S_IN), 2, "");
 		if (curr_cmd->exec.fd_out == 1 && main_struct->cmd_count != i)
 			err_chk(dup2((*p)[CUR][P_IN], S_OUT), 2, "");
 		else
 			err_chk(dup2(curr_cmd->exec.fd_out, S_OUT), 2, "");
 		close ((*p)[CUR][P_OUT]);
 		close ((*p)[PREV][P_IN]);
-		ft_exec_child(main_struct, curr_cmd, path);
+		ft_exec_child(main_struct, curr_cmd);
 	}
+	close ((*p)[PREV][P_OUT]);
+	close ((*p)[CUR][P_IN]);
+	if (i == 1 || curr_cmd->has_heredoc == 2)
+		close ((*p)[PREV][P_IN]);
+	if (curr_cmd->has_heredoc == 2 && id != 0)
+		close (g_pipe_heredoc[curr_cmd->cmd_index + 1][P_OUT]);
 }
 
 int	exec(struct	s_main *main_struct)
 {
-	int i = 0;
-	int	build_return;
-	int id = 1;
-	int p[2][2];
-	struct s_cmd_info	*curr_cmd;
-	char	*path = NULL;
+	int					i = 0;
+	int					build_return;
+	int					id = 1;
+	int					p[2][2];
 
 	build_return = -1;
 	err_chk(pipe(p[CUR]), 1, "");
 	while (i < main_struct->cmd_count)
 	{
 		i++;
-		curr_cmd = &main_struct->cmd_struct_arr[i - 1];
 		err_chk(pipe(p[CUR]), 1, "");
-		if (curr_cmd->exec.exec_line[0] && is_builtin(curr_cmd->exec.exec_line[0]) == EXIT_BUILD && main_struct->cmd_count == 1)
+		if (&main_struct->cmd_struct_arr[i - 1].exec.exec_line[0] && is_builtin(main_struct->cmd_struct_arr[i - 1].exec.exec_line[0]) == EXIT_BUILD && main_struct->cmd_count == 1)
 		{
-			ft_exit(curr_cmd->exec.exec_line, 0, main_struct);
-			continue; //???
+			ft_exit(main_struct->cmd_struct_arr[i - 1].exec.exec_line, 0, main_struct);
+			continue ;
 		}
-		if (check_buildin_fork(curr_cmd) == 0 && curr_cmd->set_file_err == 0 &&  main_struct->cmd_count == 1)
-			build_return =  exec_builtin(main_struct, curr_cmd, is_builtin(curr_cmd->exec.exec_line[0]));
+		if (check_buildin_fork(&main_struct->cmd_struct_arr[i - 1]) == 0 && main_struct->cmd_struct_arr[i - 1].set_file_err == 0 && main_struct->cmd_count == 1)
+			build_return = exec_builtin(main_struct, &main_struct->cmd_struct_arr[i - 1], is_builtin(main_struct->cmd_struct_arr[i - 1].exec.exec_line[0]));
 		signal(SIGINT, sigint_handler_in_process);
 		signal(SIGQUIT, sigquit_handler_in_process);
-		if (check_buildin_fork(curr_cmd) == 1 || main_struct->cmd_count > 1)
+		if (check_buildin_fork(&main_struct->cmd_struct_arr[i - 1]) == 1 || main_struct->cmd_count > 1)
 			id = fork();
 		err_chk(id, 1, "");
-		ft_child(main_struct, &p, i, id, curr_cmd, path);
-		close (p[PREV][P_OUT]);
-		close (p[CUR][P_IN]);
-		if (i == 1 || curr_cmd->has_heredoc == 2)
-			close (p[PREV][P_IN]);
-		if (curr_cmd->has_heredoc == 2 && id != 0)
-			close (g_pipe_heredoc[curr_cmd->cmd_index + 1][P_OUT]);
+		ft_child(main_struct, &p, i, id, &main_struct->cmd_struct_arr[i - 1]);
 	}
 	close (p[CUR][P_OUT]);
 	close (p[PREV][P_IN]);
 	waitpid(id, &i, 0);
-	while (wait(NULL) != -1);
+	while (wait(NULL) != -1)
+		;
 	signals_handeler();
 	if (build_return >= 0)
 		return (build_return);
-	return (WEXITSTATUS(i)); //check if exited with WIFSIGNALED or  WIFSTOPPED
+	return (WEXITSTATUS(i));
 }
